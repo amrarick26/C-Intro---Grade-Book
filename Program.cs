cefa131843ea39dc34f3443b091259a3cc8f5ed6
Alexa Rarick <ararick@four51.com>
2018-01-29 16:59:43 -0600

    add system.speech.synthesis class

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Speech.Synthesis;
 using System.Threading.Tasks;
 
 
@@ -11,12 +12,14 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            SpeechSyntesizer synth = new SpeechSyntesizer();
+
+            synth.Speak("Hello!");
+
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
             book.AddGrade(75);
-
-            
 
             GradeStatistics stats = book.ComputeStatistics();
 
